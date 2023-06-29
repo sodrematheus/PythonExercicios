bcbea91bbca65069175c82b2121df28686f17ffb
sodrematheus <matheussouzasodre@gmail.com>
2023-06-29 16:09:06 -0300

    1º

---
+++ ex061 - Progrressão Aritmética v2.0.py
@@ -0,0 +1,10 @@
+print('Calculando PA (Progressão aritmética) dos 10 primeiros termos\n')
+p = int(input('Digite o primeiro termo: '))
+r = int(input('Digite a razão: '))
+termo = p
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += r
+    cont += 1
+print('Fim')
