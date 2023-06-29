bfee02d1213ef2d687498ebf330bb2bb33a097f9
sodrematheus <matheussouzasodre@gmail.com>
2023-06-29 02:54:25 -0300

    1º

--- ex021.py
+++ ex021.py
@@ -1,7 +1,7 @@
 #Tocando uma música
 import pygame
 pygame.init()
-pygame.mixer.music.load('Maresia.mp3')
+pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
