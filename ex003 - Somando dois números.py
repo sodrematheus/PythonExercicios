e7c32f21f7fb9886dd6c9d94836787cce9b4f7d8
sodrematheus <matheussouzasodre@gmail.com>
2023-08-30 11:21:01 -0300

    1º

--- ex003 - Somando dois números.py
+++ ex003 - Somando dois números.py
@@ -1,5 +1,5 @@
 print('Informe dois números que te darei a soma deles.')
-numero_1 =int(input('Digite um número: '))
+numero_1 = int(input('Digite um número: '))
 numero_2 = int(input('Digite outro número: '))
 soma = numero_1+numero_2
 print('A soma entre {} e {} vale: {}'.format(numero_1, numero_2, soma))
